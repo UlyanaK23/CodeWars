6429854d13406ee0b2680aeee20318579a368627
UlyanaK23 <139229601+UlyanaK23@users.noreply.github.com>
2023-08-07 01:50:04 +0300

    Create abbreviate.js

---
+++ abbreviate.js
@@ -0,0 +1,8 @@
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+// The output should be two capital letters with a dot separating them.
+
+function abbrevName(name) {
+    // Конвертируем строку в массив
+    const newArray = name.split(' ')
+    return (newArray[0][0] + '.' + newArray[1][0]).toUpperCase()
+}
